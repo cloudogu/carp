71580faf5aacabe054c9cce46e4e4b82f3f37f0b
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-05-22 16:52:52 +0200

    #1 Use request URL instead of URI due to tests

--- logouthandler.go
+++ logouthandler.go
@@ -32,8 +32,8 @@ func (h *LogoutRedirectionHandler) ServeHTTP(w http.ResponseWriter, r *http.Requ
 }
 
 func (h *LogoutRedirectionHandler) isLogoutRequest(r *http.Request) bool {
-	logrus.Infof("Inspecting request %s url %s", r.Method, r.RequestURI)
+	logrus.Infof("Inspecting request %s url %s", r.Method, r.URL)
 	return (h.logoutMethod != "" || h.logoutPath != "") &&
 		(h.logoutMethod == "" || r.Method == h.logoutMethod) &&
-		(h.logoutPath == "" || h.logoutPath != "" && strings.HasSuffix(r.RequestURI, h.logoutPath))
+		(h.logoutPath == "" || h.logoutPath != "" && strings.HasSuffix(r.URL.Path, h.logoutPath))
 }
