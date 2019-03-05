a8df02c47077c00feed65de7c8fe15f2b9242ee7
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2019-03-05 10:18:31 +0100

    Go fmt code

--- configuration.go
+++ configuration.go
@@ -1,11 +1,12 @@
 package carp
 
 import (
-    "os"
-    "strings"
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
-    "github.com/pkg/errors"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
 )
 
 type Configuration struct {
@@ -22,31 +23,31 @@ type Configuration struct {
 }
 
 func ReadConfiguration() (Configuration, error) {
-    configuration := Configuration{}
+	configuration := Configuration{}
 
-    confPath := "carp.yml"
-    if len(os.Args) > 1 {
-        for _, arg := range os.Args[1:] {
-            if ! strings.HasPrefix(arg, "-") {
-                confPath = arg
-                break
-            }
-        }
-    }
+	confPath := "carp.yml"
+	if len(os.Args) > 1 {
+		for _, arg := range os.Args[1:] {
+			if !strings.HasPrefix(arg, "-") {
+				confPath = arg
+				break
+			}
+		}
+	}
 
-    if _, err := os.Stat(confPath); os.IsNotExist(err) {
-        return configuration, errors.Errorf("could not find configuration at %s", confPath)
-    }
+	if _, err := os.Stat(confPath); os.IsNotExist(err) {
+		return configuration, errors.Errorf("could not find configuration at %s", confPath)
+	}
 
-    data, err := ioutil.ReadFile(confPath)
-    if err != nil {
-        return configuration, errors.Wrapf(err, "failed to read configuration %s", confPath)
-    }
+	data, err := ioutil.ReadFile(confPath)
+	if err != nil {
+		return configuration, errors.Wrapf(err, "failed to read configuration %s", confPath)
+	}
 
-    err = yaml.Unmarshal(data, &configuration)
-    if err != nil {
-        return configuration, errors.Wrapf(err, "failed to unmarshal configuration %s", confPath)
-    }
+	err = yaml.Unmarshal(data, &configuration)
+	if err != nil {
+		return configuration, errors.Wrapf(err, "failed to unmarshal configuration %s", confPath)
+	}
 
-    return configuration, nil
+	return configuration, nil
 }
